75ef3cd52bce349fea0196e4ed25e6be0e8ae1da
Victor Ma <vic.ma@mail.utoronto.ca>
2018-06-13 15:14:11 -0400

    Add p2life rules to prepare_tick method

--- golg.py
+++ golg.py
@@ -1,9 +1,14 @@
 import time
+import random
 from typing import List
 
 
 class TPGameOfLife:
-    """Simulates a Game of Life following 2plife rules."""
+    """Simulates a version of Life based on p2life."""
+    # Cell state codes
+    DEAD = 0
+    RED = 1
+    GREEN = 2
 
     def __init__(self, columns: int, rows: int) -> None:
         """Create TPGameOfLife object.
@@ -17,10 +22,10 @@ class TPGameOfLife:
         self.columns = columns
         self.rows = rows
 
-    def seed(self, coordinates: List[List[int]]) -> None:
-        """Set alive multiple Cells on grid."""
+    def seed(self, state, coordinates: List[List[int]]) -> None:
+        """Set state of multiple Cells on grid."""
         for coord in coordinates:
-            self.grid[coord[0]][coord[1]].alive = True
+            self.grid[coord[0]][coord[1]].state = True
             
     def prepare_tick(self) -> None:
         """Let all Cells on grid know what their next move is."""
@@ -37,12 +42,39 @@ class TPGameOfLife:
                                 0 <= n_y < self.rows):
                                 # If neighbour is not the Cell itself and
                                 # if neighbour is within grid
-                            if self.grid[n_x][n_y].state == 'red':
+                            if self.grid[n_x][n_y].state == RED:
                                 red_neighbours += 1
-                            elif self.grid[n_x][n_y].state == 'green':
+                            elif self.grid[n_x][n_y].state == GREEN:
                                 green_neighbours += 1
 
-                # TODO: rules
+                if cell.state == DEAD:  #Birth
+                    if red_neighbours == 3 and green_neighbours == 3:
+                        if random.randint(1, 2) == 1:
+                            cell.next_state == RED
+                        else:
+                            cell.next_state == GREEN
+                    elif red_neighbours == 3 and green_neighbours != 3:
+                        cell.next_state = RED
+                    elif green_neighbours == 3 and red_neighbours !=3:
+                        cell.next_state = GREEN
+                elif cell.state == RED:  # Red survival/death
+                    if 2 <= red_neighbours - green_neighbours <= 3:
+                        cell.next_state = RED
+                    elif (red_neighbours - green_neighbours == 1 and
+                          red_neighbours >= 2):
+                        cell.next_state = RED
+                    else:
+                        cell.next_state = DEAD
+                elif cell.state == GREEN: # Green survival/death
+                    if 2 <= green_neighbours - red_neighbours <= 3:
+                        cell.next_state = GREEN
+                    elif (green_neighbours - red_neighbours == 1 and
+                          green_neighbours >= 2):
+                        cell.next_state = GREEN
+                    else:
+                        cell.next_state = DEAD
+
+
 
     def tick(self) -> None:
         """Apply changes to all Cells on grid to move forward a generation."""
@@ -73,12 +105,14 @@ class TPGameOfLife:
 class Cell:
     """A cell within a grid in a TPGameOfLife"""
 
-    def __init__(state: str):
+    def __init__(self, state: int):
         """Create Cell object.
 
-        state      The state of the Cell (dead/red/green)
+        state           The state of the Cell (dead=0/red=1/green=2)
+        next_state      The state of the Cell in the next generation
         """
         self.state = state
+        self.next_state = self.state
 
 if __name__ == '__main__':
     pass
